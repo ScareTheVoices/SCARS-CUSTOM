7412df260d617b854869c7b73759b1afd3c62e05
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-03-02 09:26:18 -0500

    Revise Emerald Light Arcfang effects and conditions
    Updated effects for Emerald Light Arcfang to include special summoning from hand when a card is destroyed, negating effects and halving ATK on summon, and providing destruction protection.

--- script/c1000000036.lua
+++ script/c1000000036.lua
@@ -1,58 +1,81 @@
 --Emerald Light Arcfang
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon from hand if you control Emerald Sovereign Ritual Dragon or an Emerald Light monster
+	-- Special Summon from hand if a card(s) you control is destroyed by battle or card effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,{id,0})
+	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--If Special Summoned, target 1 face-up card your opponent controls and negate its effects
+	-- On Normal or Special Summon: target 1 monster, negate effects and halve ATK until end of turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetTarget(s.distg)
 	e2:SetOperation(s.disop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	-- Protection: banish from GY to prevent destruction by card effect while you control a Ritual Monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EFFECT_DESTROY_REPLACE)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetTarget(s.reptg)
+	e4:SetValue(s.repval)
+	e4:SetOperation(s.repop)
+	c:RegisterEffect(e4)
 end
+
 s.listed_series={0x4003}
 s.listed_names={1000000000}
 
---Filter for Special Summon condition
-function s.spfilter(c)
-	return c:IsFaceup() and (c:IsCode(1000000000) or c:IsSetCard(0x4003))
+
+-- Condition for Special Summon from hand if a card(s) you control is destroyed
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(function(tc)
+		return tc:IsPreviousControler(tp) and tc:IsPreviousLocation(LOCATION_ONFIELD)
+	end,1,nil)
 end
 
---Condition for Special Summon
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 
---Filter for targetable opponent’s face-up card
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+
+-- Filter for targetable opponent’s monster
 function s.disfilter(c)
-	return c:IsFaceup() and not c:IsDisabled()
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and not c:IsDisabled()
 end
 
---Target for negate effect
+-- Target for negate and halve ATK effect
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(s.disfilter,tp,0,LOCATION_MZONE+LOCATION_SZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(s.disfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,s.disfilter,tp,0,LOCATION_MZONE+LOCATION_SZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.disfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 
---Operation for negate effect
+-- Operation for negate and halve ATK effect
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
@@ -60,12 +83,46 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
+		local atk=tc:GetAttack()
+		if atk>0 then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetValue(math.floor(atk/2))
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e3)
+		end
 	end
+end
+
+
+-- Destruction replacement logic (from Performapal Thunderhino)
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsOnField() and c:IsControler(tp) and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
+end
+
+function s.ritualfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_RITUAL)
+end
+
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.ritualfilter,tp,LOCATION_MZONE,0,1,nil) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT|REASON_REPLACE)
 end
