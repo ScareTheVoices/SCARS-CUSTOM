d37c0ebe9abc7ce7e2b4e15b560381529a455e8d
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-03-05 13:48:40 -0500

    Refactor special summon effects for 'Emerald Light'
    Updated special summon effects for 'Emerald Light' monsters to include options for hand, deck, and graveyard.

--- script/c1000000039.lua
+++ script/c1000000039.lua
@@ -1,51 +1,97 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon 1 "Emerald Light" monster from hand or Deck
+	-- Effect 1: SS from hand/deck if you control no monsters
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x4003}
 
---Special Summon limit to "Emerald Light" monsters
-function s.splimit(e,c)
-	return not c:IsSetCard(0x4003)
-end
 
---Filter for Special Summon from hand or Deck
-function s.spfilter1(c,e,tp)
+
+
+-- Filters
+function s.handdeckfilter(c,e,tp)
 	return c:IsSetCard(0x4003) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-
---Target for Special Summon effect
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
-		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+function s.gyfilter(c,e,tp)
+	return c:IsSetCard(0x4003) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.lv5lightfilter(c)
+	return c:IsFaceup() and c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 
---Operation for Special Summon effect
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	--Apply summon restriction
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	--Special Summon from hand or Deck
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
-	if #g1>0 then
-		Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.handdeckfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
+	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	local b3=Duel.IsExistingMatchingCard(s.lv5lightfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil)
+	if chk==0 then return b1 or b2 or b3 end
+	local ops,opmap={},{}
+	if b1 then table.insert(ops,"1"); opmap[#ops]=1 end
+	if b2 then table.insert(ops,"2"); opmap[#ops]=2 end
+	if b3 then table.insert(ops,"3"); opmap[#ops]=3 end
+	local sel=Duel.SelectOption(tp,table.unpack(ops))+1
+	local op=opmap[sel]
+	e:SetLabel(op)
+	if op==1 then
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	elseif op==2 then
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	elseif op==3 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,2,tp,LOCATION_ONFIELD)
+	end
+end
+
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		-- SS from hand or Deck if you control no monsters
+		if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)>0 then return end
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.handdeckfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	elseif op==2 then
+		-- SS from GY, cannot attack directly
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #g>0 then
+			if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+				local tc=g:GetFirst()
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				tc:RegisterEffect(e1)
+			end
+		end
+	elseif op==3 then
+		-- Destroy 1 level 5+ LIGHT you control and 1 opponent card
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g1=Duel.SelectMatchingCard(tp,s.lv5lightfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g2=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if #g1>0 and #g2>0 then
+			local g=Group.CreateGroup()
+			g:Merge(g1)
+			g:Merge(g2)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
 	end
 end
