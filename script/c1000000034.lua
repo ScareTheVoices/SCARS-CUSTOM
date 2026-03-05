71a6b6f76356a8f454d9362d4203e18325ea6f7a
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-03-05 13:51:37 -0500

    Refactor Emerald Light Swordsman effects and conditions

--- script/c1000000034.lua
+++ script/c1000000034.lua
@@ -1,54 +1,106 @@
 --Emerald Light Swordsman
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon from hand if you control Emerald Sovereign Ritual Dragon or another Emerald Light monster
+	-- Destroy 1 monster you control; Special Summon this card from hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,{id,1})
-	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--When Special Summoned, gain 200 ATK for each Emerald Sovereign Ritual Dragon or Emerald Light monster you control
+	-- Set 1 "Emerald Light" Trap from Deck on summon or if destroyed by battle/effect
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id)
-	e2:SetOperation(s.atkop)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCondition(s.setcon)
+	c:RegisterEffect(e4)
+	-- Continuous ATK gain
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetValue(s.atkval)
+	c:RegisterEffect(e5)
 end
 s.listed_series={0x4003}
 s.listed_names={1000000000}
 
---Filter for Special Summon condition and ATK gain
-function s.spfilter(c)
-	return c:IsFaceup() and (c:IsCode(1000000000) or c:IsSetCard(0x4003))
+
+-- Filter for monsters you control
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsDestructable()
 end
 
---Condition for Special Summon
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+-- Filter for "Emerald Light" Traps
+function s.trapfilter(c)
+	return c:IsSetCard(0x4003) and c:IsType(TYPE_TRAP) and c:IsSSetable()
 end
 
---Operation for ATK gain effect
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+
+-- Target for destroy & Special Summon
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+
+-- Operation for destroy & Special Summon
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local ct=Duel.GetMatchingGroupCount(s.spfilter,tp,LOCATION_MZONE,0,nil)
-		if ct>0 then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(ct*200)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			c:RegisterEffect(e1)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+		if c:IsRelateToEffect(e) then
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
+end
+
+
+-- Set Trap from Deck
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.trapfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.trapfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g:GetFirst())
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+-- Condition for destroyed trigger
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT)
+end
+
+-- Continuous ATK gain
+function s.atkval(e,c)
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(function(tc)
+		return tc:IsFaceup() and (tc:IsCode(1000000000) or tc:IsSetCard(0x4003))
+	end,tp,LOCATION_MZONE,0,nil)
+	return g:GetCount()*300
 end
