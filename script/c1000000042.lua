01eff71341a5b81f8479c3bafc28fc6030667e6c
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-04-09 17:33:47 -0400

    Add files via upload

---
+++ script/c1000000042.lua
@@ -0,0 +1,77 @@
+--Emerald Light Bullet Train
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Normal Summon: Special Summon 1 "Emerald Light" monster from Deck in Defense Position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(s.e1con)
+	e1:SetTarget(s.e1tg)
+	e1:SetOperation(s.e1op)
+	c:RegisterEffect(e1)
+	-- Destroyed: Add 1 "Emerald Light" card from Deck to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetTarget(s.e2tg)
+	e2:SetOperation(s.e2op)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x4003}
+
+
+-- Filter: "Emerald Light" monster in Deck (not this card) that can be Special Summoned
+function s.monfilter(c,e,tp)
+	return c:IsSetCard(0x4003) and c:IsType(TYPE_MONSTER) and not c:IsCode(id)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+-- Filter: any "Emerald Light" card in Deck (not this card) that can be added to hand
+function s.cardfilter(c)
+	return c:IsSetCard(0x4003) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+
+-- Condition: this card must have been Normal Summoned
+function s.e1con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_NORMAL)
+end
+
+-- Target: Special Summon "Emerald Light" monster from Deck (Defense Position)
+function s.e1tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.monfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+
+-- Operation: Special Summon the chosen monster in Defense Position
+function s.e1op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.monfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENCE)
+	end
+end
+
+-- Target: Add "Emerald Light" card from Deck to hand
+function s.e2tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cardfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+-- Operation: Add the chosen card to hand
+function s.e2op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.cardfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
