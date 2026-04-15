4214a003881b75e33cbc5816df7d5e9a0c5214f2
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-04-14 21:55:06 -0400

    Refactor Gateway to the Emerald Light script

--- script/c1000000038.lua
+++ script/c1000000038.lua
@@ -11,15 +11,14 @@ function s.initial_effect(c)
 	e0:SetTarget(s.thtg)
 	e0:SetOperation(s.thop)
 	c:RegisterEffect(e0)
-	--Special Summon 1 "Mikanko" monster
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
+	--Ritual Summon 1 Ritual Monster
+	local e1=Ritual.CreateProc({handler=c,
+								lvtype=RITPROC_GREATER,
+								filter=s.ritfilter,
+								desc=aux.Stringid(id,0)})
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_SZONE+LOCATION_FZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--Send this card to the GY; add 1 "Emerald Light" monster from your GY to your hand
 	local e3=Effect.CreateEffect(c)
@@ -68,44 +67,6 @@ function s.gythop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function s.spfilter(c,e,tp)
-	return c:IsMonster() and ((c:IsSetCard(0x4003) and c:IsRitualMonster()) or c:IsCode(1000000000))
-		and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
-end
-function s.matfilter(c,sc)
-	return c~=sc and c:IsMonster() and c:GetLevel()>0 and c:IsAbleToGrave()
-end
-function s.spcheckfilter(c,e,tp)
-	if not s.spfilter(c,e,tp) then return false end
-	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,c)
-	return #mg>0 and mg:GetSum(Card.GetLevel)>=c:GetLevel()
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsExistingMatchingCard(s.spcheckfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_HAND+LOCATION_MZONE)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local tc=Duel.SelectMatchingCard(tp,s.spcheckfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
-	if tc then
-		Duel.ConfirmCards(1-tp,tc)
-		Duel.ShuffleHand(tp)
-		local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,tc)
-		if #mg==0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local minsel=tc:GetLevel()>0 and 1 or 0
-		local sg=mg:Select(tp,minsel,#mg,nil)
-		if #sg==0 then return end
-		local sum=0
-		for c in sg:Iter() do sum=sum+c:GetLevel() end
-		if sum<tc:GetLevel() then Duel.ShuffleHand(tp); return end
-		if Duel.SendtoGrave(sg,REASON_EFFECT)<=0 then return end
-		if tc:IsLocation(LOCATION_HAND) then
-			Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP)
-			Duel.SpecialSummonComplete()
-		end
-	end
+function s.ritfilter(c)
+	return c:IsRitualMonster() and (c:IsSetCard(0x4003) or c:IsCode(1000000000))
 end
