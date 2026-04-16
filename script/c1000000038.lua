1119cc30b73a999f5fc63a25917a686ff36f2391
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-04-15 20:57:22 -0400

    Refactor Gateway to the Emerald Light script

--- script/c1000000038.lua
+++ script/c1000000038.lua
@@ -20,6 +20,18 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_SZONE+LOCATION_FZONE)
 	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
+	--If an "Emerald Light" monster is Special Summoned to your field: add 1 specified monster from your Deck or banishment to your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE+LOCATION_FZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.thcon2)
+	e2:SetTarget(s.thtg2)
+	e2:SetOperation(s.thop2)
+	c:RegisterEffect(e2)
 	--Send this card to the GY; add 1 "Emerald Light" monster from your GY to your hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,3))
@@ -44,6 +56,28 @@ end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.spfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and (c:IsSetCard(0x4003) or c:IsCode(1000000000))
+end
+function s.thfilter2(c)
+	return (c:IsCode(1000000000) or (c:IsSetCard(0x4003) and c:IsRitualMonster()))
+		and c:IsAbleToHand()
+		and (not c:IsLocation(LOCATION_REMOVED) or c:IsFaceup())
+end
+function s.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spfilter,1,nil,tp)
+end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK+LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_REMOVED)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK+LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,g)
 	end
