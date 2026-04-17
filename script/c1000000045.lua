45d3fda059dd2cc437d8e19625bd596a46eb8e94
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-04-17 12:50:24 -0400

    Refactor Emerald Light Sovereign effects and conditions

--- script/c1000000045.lua
+++ script/c1000000045.lua
@@ -1,7 +1,25 @@
 --Emerald Light Sovereign
 --Made by ScareTheVoices
 local s,id=GetID()
+s.listed_series={0x4003}
+s.listed_names={1000000000}
+
+function s.selfspcon(e)
+	return e and e:GetHandler() and e:GetHandler():IsCode(id) and e:GetLabel()==id
+end
+function s.splimit(e,se,sp,st)
+	return (st&SUMMON_TYPE_RITUAL)==SUMMON_TYPE_RITUAL or s.selfspcon(se)
+end
 function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special Summon limitation
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(s.splimit)
+	c:RegisterEffect(e0)
+
 	--Special Summon from hand by destroying required cards
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -12,22 +30,54 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+
+	--Cannot summon code 1000000000 while this card is face-up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(s.splimitcode)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_SUMMON)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EFFECT_CANNOT_MSET)
+	c:RegisterEffect(e4)
+
+	--You cannot control code 1000000000 while this card is face-up
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_ADJUST)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetOperation(s.ctop)
+	c:RegisterEffect(e5)
+
+	--When this card leaves the field: revive code 1000000000 on your next Standby Phase
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_LEAVE_FIELD)
+	e6:SetProperty(EFFECT_FLAG_DELAY)
+	e6:SetCondition(s.rvcon)
+	e6:SetOperation(s.rvop)
+	c:RegisterEffect(e6)
+end
+
+function s.splimitcode(e,c)
+	return c:IsCode(1000000000)
 end
 
 function s.reqfilter(c)
-	return c:IsCode(1000000000) and c:IsDestroyable()
-end
-function s.ritfilter(c,ex)
-	return c:IsFaceup() and c:IsSetCard(0x4003) and c:IsType(TYPE_RITUAL) and c:IsDestroyable() and c~=ex
+	return c:IsFaceup() and c:IsMonster() and c:IsCode(1000000000) and c:IsDestructable()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 			and Duel.IsExistingMatchingCard(s.reqfilter,tp,LOCATION_ONFIELD,0,1,nil)
-			and Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_MZONE,0,1,nil)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,2,tp,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -37,16 +87,64 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g1=Duel.SelectMatchingCard(tp,s.reqfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
 	local tc1=g1:GetFirst()
 	if not tc1 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g2=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_MZONE,0,1,1,tc1)
-	local tc2=g2:GetFirst()
-	if not tc2 then return end
-	g1:Merge(g2)
 	Duel.HintSelection(g1,true)
-	if Duel.Destroy(g1,REASON_EFFECT)==2 and c:IsRelateToEffect(e) then
-		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+	if Duel.Destroy(g1,REASON_EFFECT)~=0 and c:IsRelateToEffect(e) then
+		e:SetLabel(id)
+		if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)>0 then
+			c:CompleteProcedure()
 			s.apply_granted_effect(c)
 		end
+		e:SetLabel(0)
+	end
+end
+
+function s.ctfilter(c,tp)
+	return c:IsFaceup() and c:IsCode(1000000000) and c:IsControler(tp) and c:IsAbleToGraveAsCost()
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_MZONE,0,nil,tp)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_RULE)
+	end
+end
+
+function s.rvcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+end
+function s.rvop(e,tp,eg,ep,ev,re,r,rp)
+	tp=e:GetHandler():GetPreviousControler()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetCountLimit(1)
+	e1:SetLabel(Duel.GetTurnCount())
+	e1:SetCondition(s.rvspcon)
+	e1:SetTarget(s.rvsptg)
+	e1:SetOperation(s.rvspop)
+	e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN,2)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.rvspcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and Duel.GetTurnCount()~=e:GetLabel()
+end
+function s.rvspfilter(c,e,tp)
+	return c:IsCode(1000000000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.rvsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.rvspfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.rvspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.rvspfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
@@ -54,15 +152,15 @@ function s.zerotg(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x4003) and c~=e:GetHandler()
 end
 function s.gainfilter(c,sc)
-	return c:IsFaceup() and c:IsSetCard(0x4003) and c~=sc
+	return c:IsFaceup() and c:IsSetCard(0x4003) and c:IsControler(sc:GetControler()) and c~=sc
 end
 function s.atkval(e,c)
-	local g=Duel.GetMatchingGroup(s.gainfilter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil,e:GetHandler())
-	return g:GetSum(Card.GetAttack)
+	local g=Duel.GetMatchingGroup(s.gainfilter,c:GetControler(),LOCATION_MZONE,0,nil,e:GetHandler())
+	return g:GetSum(Card.GetBaseAttack)
 end
 function s.defval(e,c)
-	local g=Duel.GetMatchingGroup(s.gainfilter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil,e:GetHandler())
-	return g:GetSum(Card.GetDefense)
+	local g=Duel.GetMatchingGroup(s.gainfilter,c:GetControler(),LOCATION_MZONE,0,nil,e:GetHandler())
+	return g:GetSum(Card.GetBaseDefense)
 end
 function s.indtg(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x4003) and c~=e:GetHandler()
@@ -94,7 +192,7 @@ function s.apply_granted_effect(c)
 	e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
 	c:RegisterEffect(e2,true)
 
-	--This card gains ATK/DEF of all Emerald Light monsters on the field
+	--This card gains original ATK/DEF of all other Emerald Light monsters on the field
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
