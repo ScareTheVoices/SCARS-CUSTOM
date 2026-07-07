7f67beb6ab396328393ae6ad1683bb33ad8d463a
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-07-07 01:03:24 -0400

    Add files via upload

---
+++ script/c1000000049.lua
@@ -0,0 +1,113 @@
+--Mecha Oni
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--1: Destroy cards on Ritual Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--2: Battle Actiavtion Lockdown
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(s.actcon)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--3: Quick Effect Monster Negate & Banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_ROLL)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.negcon)
+	e3:SetCost(s.negcost)
+	e3:SetTarget(s.negtg)
+	e3:SetOperation(s.negop)
+	c:RegisterEffect(e3)
+end
+
+--Checks if the summon was a valid Ritual Summon
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
+end
+
+--Filters unique EARTH Machine names in the GY
+function s.count_filter(c)
+	return c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE)
+end
+
+--Targeting / Check logic for card destruction count
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local g=Duel.GetMatchingCardGrid(s.count_filter,tp,LOCATION_GRAVE,0,nil)
+		local count=g:GetClassCount(Card.GetCode)
+		return count>0 and Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_ONFIELD)
+end
+
+--Resolution logic for card destruction up to GY count
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g_gy=Duel.GetMatchingCardGrid(s.count_filter,tp,LOCATION_GRAVE,0,nil)
+	local count=g_gy:GetClassCount(Card.GetCode)
+	if count==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g_opp=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,1,count,nil)
+	if #g_opp>0 then
+		Duel.HintSelection(g_opp)
+		Duel.Destroy(g_opp,REASON_EFFECT)
+	end
+end
+
+--Battle condition: Checks if this card is currently attacking
+function s.actcon(e)
+	return Duel.GetAttacker()==e:GetHandler()
+end
+
+--Negate Condition: Checks if opponent activated a monster effect
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+
+--Cost Filter: Other Machine monsters on field or in hand
+function s.costfilter(c,ec)
+	return c:IsRace(RACE_MACHINE) and c~=ec
+end
+
+--Cost application: Tributes 1 other Machine
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,c,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TRIBUTE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,c,c)
+	Duel.Release(g,REASON_COST)
+end
+
+--Targeting info for the Negate & Banish mechanics
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+	end
+end
+
+--Resolution logic for the Negate & Banish mechanics
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
+	end
+end
