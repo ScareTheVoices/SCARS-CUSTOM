dc3b6bbda94b6b0c404056ef2163cf9329a658a9
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-07-08 00:19:34 -0400

    Added a effect that gave Darkest-Eyes Void Dragon an addition race

--- script/c1000000047.lua
+++ script/c1000000047.lua
@@ -1,6 +1,15 @@
 --Darkest-Eyes Void Dragon
 local s,id=GetID()
 function s.initial_effect(c)
+	-- Global Condition: Always treated as a Zombie monster in Deck, Hand, Field, and GY
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_MZONE)
+	e0:SetCode(EFFECT_ADD_RACE)
+	e0:SetValue(RACE_ZOMBIE)
+	c:RegisterEffect(e0)
+
 	-- Effect 1: Absorption/Destruction Quick Effect (Targeting via Alternative Blue-Eyes logic)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
