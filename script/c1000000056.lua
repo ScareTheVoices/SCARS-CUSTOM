351ab8feea2a254330a71c0deb9b2a37748cf428
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-09-25 18:11:56 -0400

    Add custom card with negate and salvage effects
    Implement custom card with hand trap negate effect and GY salvage.

---
+++ script/c1000000056.lua
@@ -0,0 +1,110 @@
+-- Custom Card ID (Replace with your actual card ID)
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Listed Series & Names
+    s.listed_series={0x4003} -- Emerald Light
+    s.listed_names={1000000000} -- Emerald Sovereign Ritual Dragon
+    
+    -- Effect 1: Hand Trap Negate + ATK to 0
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE+CATEGORY_ATKCHANGE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCost(s.negcost)
+    e1:SetTarget(s.negtg)
+    e1:SetOperation(s.negop)
+    c:RegisterEffect(e1)
+    
+    -- Effect 2: GY Banish to salvage from GY/Banished
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id+1)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+end
+
+-- =========================================================================
+-- EFFECT 1: HAND TRAP NEGATE & ATK 0 FUNCTIONS
+-- =========================================================================
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+    Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+
+function s.cfilter(c)
+    return c:IsFaceup() and (c:IsCode(1000000000) or c:IsSetCard(0x4003))
+end
+
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsDisabled() then
+        -- Negate Effects
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_DISABLE_EFFECT)
+        e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e2)
+        
+        -- Check condition for ATK to 0 (Emerald Sovereign Ritual Dragon or Emerald Light monster)
+        if Duel.IsExisting穩定 or Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) then
+            Duel.AdjustInstantly(tc)
+            -- Change ATK to 0
+            local e3=Effect.CreateEffect(c)
+            e3:SetType(EFFECT_TYPE_SINGLE)
+            e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+            e3:SetValue(0)
+            e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e3)
+        end
+    end
+end
+
+-- =========================================================================
+-- EFFECT 2: GY / BANISHED SALVAGE FUNCTIONS
+-- =========================================================================
+function s.thfilter(c)
+    return c:IsSetCard(0x4003) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.thfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,tc)
+    end
+end
